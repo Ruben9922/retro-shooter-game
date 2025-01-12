13376120a45888fac6beb91c58e5f61b024eaff0
Ruben Dougall <Ruben9922@users.noreply.github.com>
2025-01-12 12:22:02 +0000

    Remove separate `draw_utilities` file

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -27,6 +28,20 @@ type lifeLostTickMsg time.Time
 type messageTickMsg time.Time
 
 var emptyVector2d = vector2d{x: -1, y: -1}
+
+var whiteColor = lipgloss.AdaptiveColor{
+	Light: "8",
+	Dark:  "7",
+}
+var blackColor = lipgloss.AdaptiveColor{
+	Light: "15",
+	Dark:  "0",
+}
+var accentColor = lipgloss.AdaptiveColor{
+	Light: "12",
+	Dark:  "4",
+}
+var secondaryTextStyle = help.New().Styles.ShortDesc
 
 func initialModel() model {
 	return model{
@@ -84,6 +99,14 @@ func (m model) View() string {
 		lipgloss.NewStyle().Padding(1, 1).Render(m.view.draw(m)),
 	)
 	return lipgloss.NewStyle().Height(m.windowSize.y).Render(lipgloss.JoinVertical(lipgloss.Left, titleBar, mainView))
+}
+
+func drawTitleBar(m model, title string) string {
+	horizontalPadding := 2
+	titleBarStyle := lipgloss.NewStyle().Background(whiteColor).Foreground(blackColor).Bold(true).Padding(0, horizontalPadding)
+	leftText := strings.Repeat(" ", lipgloss.Width(version))
+	centerText := lipgloss.PlaceHorizontal(m.windowSize.x-(2*lipgloss.Width(version))-(horizontalPadding*2), lipgloss.Center, strings.ToUpper(title))
+	return titleBarStyle.Render(lipgloss.JoinHorizontal(lipgloss.Top, leftText, centerText, version))
 }
 
 func main() {
